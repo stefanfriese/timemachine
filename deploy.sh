8e5bdc3c1d3ca6eaed4894a798b22791c6c66960
x <x>
2019-10-07 13:47:18 +0200

    Updated deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
 buildtag=$(git rev-parse --short HEAD)
+
+echo "Deployment Step 1/3: docker build..."
 docker build . --tag y3key/timemachine-kube:$buildtag
+
+echo "Deployment Step 2/3: docker push..."
 docker push y3key/timemachine-kube:$buildtag
-kubectl set image deployment/timemachine-deployment y3key/timemachine-kube:$buildtag
+
+echo "Deployment Step 3/3: kubectl set image..."
+kubectl set image deployment/timemachine-deployment timemachine=y3key/timemachine-kube:$buildtag
+
+echo "Deployment finished."
