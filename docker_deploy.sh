68c6cec1dbd66337acda31d359604bc5d28ba021
x <x>
2019-10-08 12:29:30 +0200

    updated deployment script

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,7 +1,7 @@
 buildtag=$(git rev-parse --short HEAD)
 
-echo "Deployment Step: docker push..."
-docker push y3key/timemachine-kube:$buildtag
+#echo "Deployment Step: docker push..."
+#docker push y3key/timemachine-kube:$buildtag
 
 echo "Deployment Step: kubectl set image..."
 kubectl set image -n default deployment/timemachine-deployment timemachine=y3key/timemachine-kube:$buildtag
